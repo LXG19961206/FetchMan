e7fb8c8a73810760a85ad818eb29ff794536210d
liuxingang <lxg13171827320@163.com>
2023-07-08 06:45:53 +0800

    add tab

--- model/tabPage.go
+++ model/tabPage.go
@@ -1,1 +1,8 @@
 package model
+
+type TabPage struct {
+	Id          int    `db:"INTEGER PRIMARY KEY AUTOINCREMENT" name:"id" json:"id"`
+	WorkplaceId int    `db:"INTEGER PRIMARY KEY AUTOINCREMENT" name:"workplace_id" json:"workplace_id"`
+	CreateTime  string `db:"DATE NULL" name:"create_time" json:"createTime"`
+	ReqId       int    `db:"INTEGER PRIMARY KEY AUTOINCREMENT" name:"req_id" json:"req_id"`
+}
