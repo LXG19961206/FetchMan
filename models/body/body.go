d41e4971f2349ed04e4eed82fa8e9b6ebe663611
liuxingang <lxg13171827320@163.com>
2024-02-15 13:53:43 +0800

    add body record

--- models/body/body.go
+++ models/body/body.go
@@ -4,18 +4,6 @@ import (
 	"changeme/models"
 )
 
-const (
-	JSON      = "application/json"
-	TEXT      = "text/plain"
-	XML       = "application/xml"
-	HTML      = "text/html"
-	JSONP     = "application/javascript"
-	MULTIPART = "multipart/form-data"
-	URL       = "application/x-www-form-urlencoded"
-	BINARY    = "application/octet-stream"
-	FILE      = "file"
-)
-
 type Body struct {
 	models.BaseFields `xorm:"extends"`
 	Type              string `json:"type"`
